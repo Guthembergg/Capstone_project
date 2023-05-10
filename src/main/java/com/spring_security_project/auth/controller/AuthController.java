9a90ee32decb738b6720140853b4ba5a10150742
Guthembergg <karimguettech@gmail.com>
2023-05-10 22:02:43 +0200

    added /profile in auth controller

--- src/main/java/com/spring_security_project/auth/controller/AuthController.java
+++ src/main/java/com/spring_security_project/auth/controller/AuthController.java
@@ -1,46 +1,58 @@
 package com.spring_security_project.auth.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.spring_security_project.auth.entity.User;
 import com.spring_security_project.auth.payload.JWTAuthResponse;
 import com.spring_security_project.auth.payload.LoginDto;
 import com.spring_security_project.auth.payload.RegisterDto;
+import com.spring_security_project.auth.repository.UserRepository;
 import com.spring_security_project.auth.service.AuthService;
-
 
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
 
-    private AuthService authService;
+	private AuthService authService;
+@Autowired
+UserRepository repo;
+	public AuthController(AuthService authService) {
+		this.authService = authService;
+	}
 
-    public AuthController(AuthService authService) {
-        this.authService = authService;
-    }
+	// Build Login REST API
+	@PostMapping(value = { "/login", "/signin" })
+	public ResponseEntity<JWTAuthResponse> login(@RequestBody LoginDto loginDto) {
 
-    // Build Login REST API
-    @PostMapping(value = {"/login", "/signin"})
-    public ResponseEntity<JWTAuthResponse> login(@RequestBody LoginDto loginDto){
-           	
-    	String token = authService.login(loginDto);
+		String token = authService.login(loginDto);
 
-        JWTAuthResponse jwtAuthResponse = new JWTAuthResponse();
-        jwtAuthResponse.setUsername(loginDto.getUsername());
-        jwtAuthResponse.setAccessToken(token);
+		JWTAuthResponse jwtAuthResponse = new JWTAuthResponse();
+		jwtAuthResponse.setUsername(loginDto.getUsername());
+		jwtAuthResponse.setAccessToken(token);
 
-        return ResponseEntity.ok(jwtAuthResponse);
-    }
+		return ResponseEntity.ok(jwtAuthResponse);
+	}
 
-    // Build Register REST API
-    @PostMapping(value = {"/register", "/signup"})
-    public ResponseEntity<String> register(@RequestBody RegisterDto registerDto){
-        String response = authService.register(registerDto);
-        return new ResponseEntity<>(response, HttpStatus.CREATED);
-    }
+	// Build Register REST API
+	@PostMapping(value = { "/register", "/signup" })
+	public ResponseEntity<String> register(@RequestBody RegisterDto registerDto) {
+		String response = authService.register(registerDto);
+		return new ResponseEntity<>(response, HttpStatus.CREATED);
+	}
+
+	@GetMapping("/profile")
+	public ResponseEntity<?> getProfile() {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		User u = repo.findByEmail(auth.getName()).get();
+		return new ResponseEntity<>(u, HttpStatus.OK);
+	}
 }
